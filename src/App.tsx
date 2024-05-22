c05f7059bd23cc1f80c2ba7df9a0ee9dbebf3244
Drodexaron <yzlen7460@gmail.com>
2024-05-22 17:30:01 +0630

    Login Page Update 2/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,163 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// FireBase Imports
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  setPersistence,
+  browserLocalPersistence,
+  sendEmailVerification,
+} from 'firebase/auth';
+import firebaseConfig from './firebaseConfig.js';
 
+// React & CSS imports
+import React, { useState, useEffect } from 'react';
+import { useRef } from 'react';
+import './App.css';
+
+// Components Imports
+import { AppMessageEmailVerification, AppMessageSuccessful, AppMessageError, AppMessageEmailResendVerification } from './components/AppMessage.jsx';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
+
+// App Render
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  // Consts & Defines
+  // Retrieve Inputs Values
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  // ALERT Window
+  const [showAppMessage, setAppMessage] = useState(false);
+  const [activeMessage, setActiveMessage] = useState(null);
+  const [error, setError] = useState(null);
+  
+  // Cooldown 
+  const [resendCooldown, setResendCooldown] = useState(null);
+  
+  // Check Persistence / Login auth
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && user.emailVerified)
+      {
+        window.location.href = '/dashboard/index.html';
+      }
+    });
+    return unsubscribe;
+  }, []);
+  
+  // FireBase Functions
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      
+      if (!user.emailVerified) {
+        await sendEmailVerification(user);
+        setAppMessage(true);
+        setActiveMessage('EmailVerification');
+      } else {
+        setAppMessage(true);
+        setActiveMessage('Successful');
+      }
+    } catch(error) {
+      setError(error);
+      setAppMessage(true);
+      setActiveMessage('Error');
+      console.error("Firebase Error:", error.code, error.message);
+    }
+  };
+  
+  // Handle Email Resend Verification
+  const handleResendVerification = async () => {
+    try {
+      if (resendCooldown) {
+        setError('Please wait 45 seconds to be able to use Resend Button.');
+        return;
+      }
+      await sendEmailVerification(auth.currentUser);
+      setAppMessage(true);
+      setActiveMessage('ResendVerification');
+      setResendCooldown(45);
+    } catch (error) {
+      setError(error.message);
+      setAppMessage(true);
+      setActiveMessage('Error');
+      console.error("Firebase Error:", error.code, error.message);
+    }
+  }
+  
+  // Cooldown Timer Effect
+  useEffect(() => {
+    let interval;
+    if (resendCooldown > 0) {
+      interval = setInterval(() => {
+        
+        setResendCooldown(resendCooldown - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    
+    return () => clearInterval(interval);
+  }, [resendCooldown]);
+  
+  // Button Trigger & Functions
+  const loginSubmit = useRef(null);
+  const triggerClick = () => {
+    if (loginSubmit.current) {
+      loginSubmit.current.click();
+    }
+  };
+  
+  // UI STRUCTURE
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <header className="App-header">
+        <h4>Task<b>Forge</b></h4>
+        <p>by SpallaX</p>
+      </header>
+      
+      <form onSubmit={handleSubmit}>
+        <h3>Login</h3>
+        <input type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+        required></input>
+        <input 
+        type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+        required></input>
+        <div id="error-Message"></div> 
+        <button type="submit">Login</button>
+      </form>
+      
+      {showAppMessage && (
+      <>
+        {activeMessage === 'EmailVerification' && <AppMessageEmailVerification  onResend={handleResendVerification} cooldown={resendCooldown}/>}
+        {activeMessage === 'Successful' && <AppMessageSuccessful />}
+        {activeMessage === 'Error' && <AppMessageError errorMessage={error}/>}
+        {activeMessage === 'ResendVerification' && <AppMessageEmailResendVerification onResend={handleResendVerification} cooldown={resendCooldown} />}
+      </>
+      )}
+      
+      <div className="footer" >
+        <p>© 2024 SpallaX. All rights reserved.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'; // Import other services as needed
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsurYBZqQnPRNCfoYLxOBc3VacOk9O-4Q",
+  authDomain: "taskforge-amelie.firebaseapp.com",
+  projectId: "taskforge-amelie",
+  storageBucket: "taskforge-amelie.appspot.com",
+  messagingSenderId: "694317972667",
+  appId: "1:694317972667:web:7d5f12c1c6f0338a437d9e",
+  measurementId: "G-TBJML4BMT3"
+};
+
+export default firebaseConfig;
